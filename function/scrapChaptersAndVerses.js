d85cdc1af05683288eca82cadf9dec930bd39b0b
SleepingNext <veroskevin8@gmail.com>
2021-03-22 18:51:46 +0700

    Add Axios to the project

--- function/scrapChaptersAndVerses.js
+++ function/scrapChaptersAndVerses.js
@@ -5,6 +5,8 @@ module.exports = async function scrapChaptersAndVerses() {
     const verses = await VerseModel.find({});
     if (chapters.length === 0 && verses.length === 0) {
         console.log("Scraping Chapters and Verses...");
+
+        
         await ChapterModel.insertMany([], null);
         console.log("Done scraping Chapters and Verses.");
     }
